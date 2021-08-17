74b426abf5747e3d55b79aed7205c4178d4db380
Will Holmes <willholmes97@icloud.com>
2021-08-17 20:33:55 +0100

    added trivial mobile menu

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 interface HeaderProps {
 
 }
 
 export const Header = (props: HeaderProps) => {
+    const [ mobileMenuToggled, setMobileMenuToggled ] = useState(false);
     return (
         <header>
             <nav className="container flex items-center py-4 mt-4 sm:mt-12">
@@ -18,8 +19,16 @@ export const Header = (props: HeaderProps) => {
                     <li className="cursor-pointer">Features</li>
                     <button type="button" className="bg-tbpp-red text-white rounded-md px-7 py-3 uppercase">Try it out!</button>
                 </ul>
-                <div className="flex sm:hidden flex-1 justify-end"><i className="text-2xl fas fa-bars"></i></div>
+                <div className="flex sm:hidden flex-1 justify-end" onClick={() => setMobileMenuToggled(!mobileMenuToggled)}><i className="text-2xl fas fa-bars"></i></div>
             </nav>
+            
+            <div className={`shadow rounded-md font-semibold ${mobileMenuToggled ? '' : 'hidden'} sm:hidden`}>
+                <ul>
+                    <a href="" className="block py-2 px-4 text-sm hover:bg-gray-200">About</a>
+                    <a href="" className="block py-2 px-4 text-sm hover:bg-gray-200">Features</a>
+                    <a href="" className="block py-2 px-4 text-sm hover:bg-gray-200">Try it out!</a>
+                </ul>
+            </div>
         </header>
     )
 }
