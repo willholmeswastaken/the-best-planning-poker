b58ebc2f827e77d6d35ce30e1f885baa6cfb0da6
Will Holmes <willholmes97@icloud.com>
2021-08-17 21:09:17 +0100

    Setup arch to allow viewings of rooms and creation of rooms

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import { LandingPage } from './pages/landing';
+import { CreateRoomPage } from './pages/create-room';
+import { ViewRoomPage } from './pages/view-room';
 
 const App = () => (
   <div>
     <Switch>
       <Route exact path="/" component={LandingPage} />
+      <Route exact path="/create-room" component={CreateRoomPage} />
+      <Route exact path="/view-room" component={ViewRoomPage} />
       <Route component={LandingPage} />
     </Switch>
   </div>
---
+++ src/pages/view-room/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+export const ViewRoomPage = (props: Props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
