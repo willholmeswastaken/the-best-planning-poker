8708992c621569e57f523c5786de7d77e00435ac
Will Holmes <willholmes97@icloud.com>
2021-08-17 21:47:38 +0100

    Started to implement formik to get an idea for how this will look like, still need to add validation but getting there...

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,18 +1,21 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 
 interface HeaderProps {
 
 }
 
 export const Header = (props: HeaderProps) => {
-    const [ mobileMenuToggled, setMobileMenuToggled ] = useState(false);
+    const [mobileMenuToggled, setMobileMenuToggled] = useState(false);
     return (
         <header>
             <nav className="container flex items-center py-4 mt-4 sm:mt-12">
                 <div className="py-1">
-                    <h2 className="text-tbpp-blue text-md md:text-lg lg:text-xl text-center lg:text-left mb-6">
-                        The Best Planning Poker
-                    </h2>
+                    <Link to="/">
+                        <h2 className="text-tbpp-blue text-md md:text-lg lg:text-xl text-center lg:text-left mb-6">
+                            The Best Planning Poker
+                        </h2>
+                    </Link>
                 </div>
                 <ul className="hidden sm:flex flex-1 justify-end items-center gap-12 text-tbpp-blue uppercase text-xs">
                     <li className="cursor-pointer">About</li>
@@ -21,7 +24,7 @@ export const Header = (props: HeaderProps) => {
                 </ul>
                 <div className="flex sm:hidden flex-1 justify-end" onClick={() => setMobileMenuToggled(!mobileMenuToggled)}><i className="text-2xl fas fa-bars"></i></div>
             </nav>
-            
+
             <div className={`shadow rounded-md font-semibold ${mobileMenuToggled ? '' : 'hidden'} sm:hidden`}>
                 <ul>
                     <a href="" className="block py-2 px-4 text-sm hover:bg-gray-200">About</a>
